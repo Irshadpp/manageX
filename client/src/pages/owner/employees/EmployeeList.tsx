1735b632788deca22ff908cd69372bf1d048b148
msys <irshad89434@gmail.com>
2024-09-05 21:01:17 +0530

    created employeesSlice and employees cards rendered with data

--- client/src/pages/owner/employees/EmployeeList.tsx
+++ client/src/pages/owner/employees/EmployeeList.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import { RootState } from '@/store';
+import { fetchEmployees } from '@/store/employeeThunks';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import EmployeeCard from './EmployeeCard';
 
 const EmployeeList = () => {
+
+  const dispatch = useDispatch<any>();
+  const {error, loading, employees} = useSelector((state: RootState) => state.employee);
+  console.log("------------",employees, loading)
+  useEffect(()=>{
+    dispatch(fetchEmployees());
+  },[dispatch]);
+
+  if(loading){
+    return <h1>loading...</h1>
+  }
+
+  if(error){
+    return <h1>{error}</h1>
+  }
+
   return (
-    <div>
-      employee list
-    </div>
+    <>
+    {employees ? (
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-2">
+        {employees.map(employee =>(
+          <EmployeeCard key={employee.id} employee={employee}/>
+        ))}
+      </div>
+    ) : (
+      <div>
+        <h1>employee weren't created yet</h1>
+      </div>
+    )}
+    </>
   )
 }
 
---
+++ client/src/store/employeeSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Employee, initialState } from "./types/employee";
+
+
+const employeeSlice = createSlice({
+    name: "employee",
+    initialState: initialState,
+    reducers:{
+        fetchEmployeesRequest(state){
+            state.loading = true;
+        },
+        fetchEmployeesSuccess(state, action: PayloadAction<Employee[]>){
+            console.log(action.payload, "-=-=-=-=-=-=--=-=-=-=-=-=-=-=")
+            state.employees = action.payload;
+            state.loading = false;
+            state.error = null;
+            state.shouldRefetch = false;
+        },
+        fetchEmployeeFailure(state, action: PayloadAction<string>){
+            state.loading = false;
+            state.error = action.payload;
+        }
+
+    }
+});
+
+export const {
+    fetchEmployeesRequest,
+    fetchEmployeesSuccess,
+    fetchEmployeeFailure,
+} = employeeSlice.actions;
+
+export default employeeSlice.reducer;
