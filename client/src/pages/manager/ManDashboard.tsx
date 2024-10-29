0515c8ec19825e66aa082a1dcbb6f68bde58c2fd
msys <irshad89434@gmail.com>
2024-10-29 22:49:10 +0530

    feat: add dashboard on manager side

--- client/src/pages/manager/ManDashboard.tsx
+++ client/src/pages/manager/ManDashboard.tsx
@@ -1,10 +1,21 @@
+import NewTaskChart from '@/components/charts/NewTaskChart'
+import ProjectsDone from '@/components/charts/ProjectsDone'
+import TaskCompleteChart from '@/components/charts/TaskCompleteChart'
+import TaskDone from '@/components/charts/TaskDone'
 import React from 'react'
 
 const ManDashboard = () => {
   return (
-    <div>
-      manager ManDashboard;
-    </div>
+    <div className="min-h-screen w-full p-5">
+    <div className="col-span-3">
+     <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+       <TaskCompleteChart />
+       <NewTaskChart />
+       <ProjectsDone />
+     </div>
+     <TaskDone />
+   </div>
+ </div>
   )
 }
 
